bfe9407ea02e7fa98dbe5df527f15b561688e0db
Zhadi <zhadi000s@gmail.com>
2023-09-20 21:53:07 +0600

    make a method to get a permission and load contacts

--- PhoneBook/src/app/pages/dashboard/dashboard.page.ts
+++ PhoneBook/src/app/pages/dashboard/dashboard.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Contacts, ContactsPlugin} from '@capacitor-community/contacts';
+import {Contacts,} from '@capacitor-community/contacts';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,9 +10,39 @@ export class DashboardPage implements OnInit {
 
   contactsList! : any[];
 
-  constructor(private contacts: ContactsPlugin) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
+  async requestPermission(){
+    try{
+      const permission: any = await Contacts.requestPermissions();
+      if(permission === 'granted'){
+        this.loadContacts();
+      }
+      else{
+        console.warn('ContactsPermission Denied');
+      }
+    }
+    catch(error){
+      console.error(error);
+    }
+  }
+
+  async loadContacts(){
+    try{
+      const result = await Contacts.getContacts({
+        projection: {
+          name:true,
+          phones: true,
+          postalAddresses: true,
+        },
+      });
+      this.contactsList = result.contacts;
+    }
+    catch (error){
+      console.error(error)
+    }
+  }
 }
